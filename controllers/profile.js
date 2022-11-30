ba1ceccf65eb3d6b9056f6535f909f84fa77c63d
Tyler Wagner <t_wagner_27@hotmail.com>
2022-11-30 11:06:45 -0500

    implemented crud operations for user, updated security features

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,16 +1,102 @@
-const handleProfileGet = (req, res, db) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({id})
-      .then(user => {
-        if (user.length) {
-          res.json(user[0])
-        } else {
-          res.status(400).json('not found')
-        }
-      })
-      .catch(err => res.status(400).json('error getting user'))
-  }
+exports.handleGetAllProfiles = (req, res, db) => {
+  db.select('*').from('users').then(users => {
+    if (users.length) {
+      res.json(users)
+    } else {
+      res.status(400).json('not found')
+    }
+  })
+.catch(err => res.status(400).json('error getting user'))
+};
 
-module.exports = {
-    handleProfileGet
+exports.handleProfileGet = (req, res, db) => {
+  const { id } = req.params;
+  db.select('*').from('users').where({id})
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('not found')
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'))
+};
+
+exports.handleProfileUpdate = async (req, res, db) => {
+  const changes = {};
+  if (req.body.name) changes.name = req.body.name;
+  if (req.body.email) changes.email = req.body.email
+
+  if (Object.keys(changes).length > 0) {
+    const userEmail = (await (await db('users').where({id: req.params.id}))[0]).email;
+    if (changes.email) {
+      await db('login')
+      .update({email: changes.email})
+      .where({ email: userEmail})
+      .catch((err) => console.log(err))
+    }
+    await db('users')
+      .update(changes)
+      .where({ id: req.params.id })
+      .catch((err) => console.log(err)) 
+  } else {
+    res.status(400).json({status: 'error', message: 'Must submit valid changes'})
+  }
+  const user = await db('users').where({id: req.params.id}).select('*')
+  res.status(200).json({
+    status: 'success',
+    user
+  })
+};
+
+exports.handleProfileDelete = (req, res, db) => {
+  db('users').update({ active: false }).where({id: req.params.id}).catch((err) => console.log(err))
+  
+  res.status(204).json({
+    status: 'success',
+    data: null
+  })
+};
+
+exports.handleProfileChangePassword = (req, res, db, bcrypt) => {
+  const { currentPassword, newPassword, email } = req.body;
+  db.select('email', 'hash').from('login')
+      .where('email', '=', email)
+      .then(data => {
+        const isValid = bcrypt.compareSync(currentPassword, data[0].hash);
+        if (isValid) {
+          const newHash = bcrypt.hashSync(newPassword);
+          db('login').update({ hash: newHash }).where({email}).catch((err) => console.log(err))
+          return res.status(200).json({status: 'success'})
+        } else {
+          res.status(400).json('wrong credentials')
+        }
+  
+      })
+      .catch(err => res.status(400).json('wrong credentials'))
 }
+
+exports.handleGetSelf = (req, res, db) => {
+
+  const { id } = req.params;
+  db.select('*').from('users').where({id})
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('not found')
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'))
+};
+ 
+exports.handleDeleteSelf = (req, res, db) => {
+  const { email } = req.body;
+
+  db('login').update({active: false}).where({email: email}).catch((err) => console.log(err))
+  
+  res.status(204).json({
+    status: 'success',
+    data: null
+  })
+};
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
 const knex = require('knex');
 const bcrypt = require('bcrypt-nodejs');
-
+const hpp = require('hpp');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
@@ -20,15 +23,42 @@ const db = knex({
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.options('*', cors());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use(limiter);
+
+app.use(express.json({ limit: '10kb' }));
+
+app.use(xss())
+app.use(hpp())
+
 app.get('/', (req, res) => { res.send('Success') });
 
+// Authorization
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
+
+app.get('/profile', (req, res) => { profile.handleGetAllProfiles(req, res, db)})
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
+app.patch('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db)})
+app.patch('/changePassword', (req, res) => { profile.handleProfileChangePassword(req, res, db, bcrypt)});
+app.delete('/profile/:id', (req, res) => { profile.handleProfileDelete(req, res, db)})
+
+app.get('/getMe', (req, res) => { profile.handleGetSelf(req, res, db)})
+app.delete('/deleteMe', (req, res) => { profile.handleDeleteSelf(req, res, db)})
+
+//Clarifai API Call
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+
 app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running on port ${process.env.PORT}`);
+  console.log(`app is running on port ${process.env.PORT ? process.env.PORT : 3000}`);
 })
